c70543729cd1c57c07896d6e7fbc6c62b9496428
Salem Alswil <sa.al9wel@gmail.com>
2026-04-23 03:41:04 +0300

    fixing typescript mistake

--- src/features/users/hooks/useCreateUser.ts
+++ src/features/users/hooks/useCreateUser.ts
@@ -1,6 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { userApi } from "../api/userApi"
-import type { User } from "@/types/User"
 
 export const useCreateUser = () => {
     const queryClient = useQueryClient()
